c404454a62d893b16151a0824595f39ea5df3c37
Thranae <thranae7@gmail.com>
2026-08-05 04:51:23 +0530

    Remove stackable glass background from Trips and Documents

--- client/src/pages/documents/DocumentsPage.jsx
+++ client/src/pages/documents/DocumentsPage.jsx
@@ -25,17 +25,6 @@ export default function DocumentsPage() {
   const { addToast } = useToast();
   return (
     <PageTransition className="relative min-h-full">
-      {/* 3D Stackable Glass Background */}
-      <div className="fixed inset-0 z-0 pointer-events-none opacity-50 mix-blend-screen overflow-hidden">
-        <iframe 
-          src="https://my.spline.design/stackableglass-31A0B3Kcmae5ah8U898DTaXT-dVR/" 
-          frameBorder="0" 
-          width="100%" 
-          height="100%"
-          title="Stackable Glass Background"
-        ></iframe>
-      </div>
-      
       <div className="relative z-10 w-full pb-20">
       {/* Header */}
       <div className="flex flex-col sm:flex-row sm:items-end justify-between gap-6 mb-10">
